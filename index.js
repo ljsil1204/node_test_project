30496d823c7d6dbc03705091340eb8a35e031019
이진실 <79292184+ljsil1204@users.noreply.github.com>
2023-04-10 18:20:26 +0900

    hellow world

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const fs = require("fs");
+const http = require("http");
 
-const data = fs.readFileSync("./data.txt", "utf-8");
+const hostname = "127.0.0.1";
+const port = 3000;
 
-console.log(data);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hellow World\n");
+});
+
+server.listen(port, hostname, () =>
+  console.log(`Server running at http://${hostname}:${port}/`)
+);
+
+// curl - http 요청을 보낼수 있는 커맨드라인 명령어
+// curl -X GET 'localhost:3000'
