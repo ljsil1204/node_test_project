e06bc662987ec53965763c832853dca1437bebe7
이진실 <79292184+ljsil1204@users.noreply.github.com>
2023-04-10 18:29:26 +0900

    hellow world 코드 주석 설명 추가

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// http 모듈
 const http = require("http");
 
+// 서버주소와 포트 설정
 const hostname = "127.0.0.1";
 const port = 3000;
 
@@ -9,9 +11,12 @@ const server = http.createServer((req, res) => {
   res.end("Hellow World\n");
 });
 
+// listen - 서버를 요청 대기 상태로 만들어주는 함수 => 서버가 클라이언트의 요청을 받기 위해서 종료하지 않고 대기하는 상태
 server.listen(port, hostname, () =>
+  // 서버구동 시 실행되는 함수
   console.log(`Server running at http://${hostname}:${port}/`)
 );
 
 // curl - http 요청을 보낼수 있는 커맨드라인 명령어
 // curl -X GET 'localhost:3000'
+// curl -X GET '127.0.0.1:3000'
