b911d6f20e6b50c4265c742b2e3a5da6c7aed2e4
이진실 <79292184+ljsil1204@users.noreply.github.com>
2023-05-18 16:21:06 +0900

    써드파티 미들웨어 사용해보기
    1. 모듈 설치
    2. 인스턴스 생성
    3. app.use에 담어서 사용하기

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// espress => 설치명령어 : npm install express
 const express = require("express");
-const app = express(); // express 어플리케이션 => 인스턴스
+
+// morgan => 설치명령어 : npm install morgan
+//https://www.npmjs.com/package/morgan => 설명서
+const morgan = require("morgan");
+
+// express 어플리케이션 => 인스턴스 만들기
+const app = express();
 
 // 미들웨어 추가 시 사용 => use()
 const logger = (req, res, next) => {
@@ -13,9 +20,14 @@ const logger2 = (req, res, next) => {
   next();
 };
 
-app.use(logger); // 미들웨어 함수
+// 미들웨어 함수
+app.use(logger);
 app.use(logger2);
 
-app.listen(3000, () => {
+//써드파티 미들웨어 사용하기
+// require로 불러온 후, app.use 함수로 불러온다.
+app.use(morgan("dev")); //출력결과예시 : GET / 404 3.726 ms - 139
+
+app.listen(3001, () => {
   console.log("Server is running");
 });
